757bed9cda3e2cf60dbb2a6aa52ac207598b1d23
Clarence <quacky888@gmail.com>
2025-02-27 11:17:13 +0700

    Update ExtractMethod.java

--- src/exercise/ExtractMethod.java
+++ src/exercise/ExtractMethod.java
@@ -17,25 +17,36 @@ public class ExtractMethod {
 		this.name = name;
 		orders = new Order();
 	}
+	
+	double outstanding = 0.0;
+	
+	public void printOwers() {
+		Enumeration elements = orders.elements();
+		
+		while (elements.hasMoreElements()) {
+		    Order each = (Order) elements.nextElement();
+		    outstanding += each.getAmount();
+		  }
+		
+		
+	}
+	
+	public void printDetails() {
+		System.out.println("name: " + name);
+		System.out.println("amount: " + outstanding);
+	}
+	
 	// TODO: reduce this method with extract method
 	void printOwing() {
-	  Enumeration elements = orders.elements();
-	  double outstanding = 0.0;
+	  
 
 	  // print banner
 	  System.out.println ("*****************************");
 	  System.out.println ("****** Customer totals ******");
 	  System.out.println ("*****************************");
-
-	  // print owings
-	  while (elements.hasMoreElements()) {
-	    Order each = (Order) elements.nextElement();
-	    outstanding += each.getAmount();
-	  }
-
-	  // print details
-	  System.out.println("name: " + name);
-	  System.out.println("amount: " + outstanding);
+	  
+	  printOwers();
+	  printDetails();
 	}
 	
 	class Order implements Enumeration {
@@ -73,3 +84,4 @@ public class ExtractMethod {
 		
 	}
 }
+
