59943c37ef59569252a69f12143e1873508ece38
Clarence <quacky888@gmail.com>
2025-02-27 11:20:15 +0700

    Update MethodObject.java

--- src/exercise/MethodObject.java
+++ src/exercise/MethodObject.java
@@ -3,22 +3,44 @@ package exercise;
 public class MethodObject {
 
 	class Account {
-		private int delta() {
-			return 10;
-		}
 		// TODO: replace this gamma method with method object (make a class for the gamma logic)  
 		public int gamma(int inputVal, int quantity, int yearToDate) {
-			    int importantValue1 = (inputVal * quantity) + delta();
-			    int importantValue2 = (inputVal * yearToDate) + 100;
-			    if ((yearToDate - importantValue1) > 100) {
-			      importantValue2 -= 20;
-			    }
-			    int importantValue3 = importantValue2 * 7;
-			    // and so on�
-			    return importantValue3 - 2 * importantValue1;
+			    return new GammaCalc(this).Calc(inputVal, quantity, yearToDate);
 			  }
 
 	}
+	
+	class GammaCalc{
+		private int inputVal;
+		private int quantity;
+		private int yearToDate;
+		
+		public GammaCalc(int inputVal, int quantity, int yearToDate) {
+			this.inputVal = inputVal;
+			this.quantity = quantity;
+			this.yearToDate = yearToDate;
+		}
+		
+		public GammaCalc(Account account) {
+			// TODO Auto-generated constructor stub
+		}
+
+		private int delta() {
+			return 10;
+		}
+		
+		public int Calc(int inputVal, int quantity, int yearToDate) {
+		    int importantValue1 = (inputVal * quantity) + delta();
+		    int importantValue2 = (inputVal * yearToDate) + 100;
+		    if ((yearToDate - importantValue1) > 100) {
+		      importantValue2 -= 20;
+		    }
+		    int importantValue3 = importantValue2 * 7;
+		    // and so on�
+		    return importantValue3 - 2 * importantValue1;
+		  }
+	}
+	
 	private void test() {
 		Account account = new Account();
 		System.out.println(account.gamma(15,10,2010));
