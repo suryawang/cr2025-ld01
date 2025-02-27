2205935315533c53905df57dd56ae25437270e3b
SteveAlden-byte <140861404+SteveAlden-byte@users.noreply.github.com>
2025-02-27 12:18:07 +0700

    Update PreserveObject.java

--- src/exercise/PreserveObject.java
+++ src/exercise/PreserveObject.java
@@ -1,71 +1,67 @@
 package exercise;
 
 public class PreserveObject {
-	class Room {
-		private int lowest, highest;
 
-		public Room(int lowest, int highest) {
-			this.lowest = lowest;
-			this.highest = highest;
-		}
+    class Room {
+        private int lowest, highest;
 
-		// TODO: replace low & high in this withinPlan with preserve whole object
-		public boolean withinPlan(HeatingPlan plan) {
-			int low = getLowestTemp();
-			int high = getHighestTemp();
-			return plan.withinRange(low, high);
-		}
+        public Room(int lowest, int highest) {
+            this.lowest = lowest;
+            this.highest = highest;
+        }
 
-		private int getHighestTemp() {
-			return highest;
-		}
+        public boolean withinPlan(HeatingPlan plan) {
+            return plan.withinRange(new TempRange(lowest, highest));
+        }
 
-		private int getLowestTemp() {
-			return lowest;
-		}
+        private int getHighestTemp() {
+            return highest;
+        }
 
-	}
+        private int getLowestTemp() {
+            return lowest;
+        }
+    }
 
-	class HeatingPlan {
-		private TempRange range;
+    class HeatingPlan {
+        private TempRange range;
 
-		public HeatingPlan(int from, int to) {
-			range = new TempRange(from, to);
-		}
+        public HeatingPlan(int from, int to) {
+            range = new TempRange(from, to);
+        }
 
-		public boolean withinRange(int low, int high) {
-			return (low >= range.getLow() && high <= range.getHigh());
-		}
-	}
+        public boolean withinRange(TempRange roomRange) {
+            return (roomRange.getLow() >= range.getLow() && roomRange.getHigh() <= range.getHigh());
+        }
+    }
 
-	class TempRange {
-		private int low;
-		private int high;
+    class TempRange {
+        private int low;
+        private int high;
 
-		public TempRange(int low, int high) {
-			this.low = low;
-			this.high = high;
-		}
+        public TempRange(int low, int high) {
+            this.low = low;
+            this.high = high;
+        }
 
-		public int getLow() {
-			return low;
-		}
+        public int getLow() {
+            return low;
+        }
 
-		public int getHigh() {
-			return high;
-		}
-	}
+        public int getHigh() {
+            return high;
+        }
+    }
 
-	public static void main(String[] args) {
-		PreserveObject p = new PreserveObject();
-		p.test();
-	}
+    public static void main(String[] args) {
+        PreserveObject p = new PreserveObject();
+        p.test();
+    }
 
-	private void test() {
-		Room r = new Room(15, 34);
-		System.out.println("Within 20-30: " + r.withinPlan(new HeatingPlan(20, 30)));
-		System.out.println("Within 25-34: " + r.withinPlan(new HeatingPlan(25, 34)));
-		System.out.println("Within 10-40: " + r.withinPlan(new HeatingPlan(10, 40)));
-	}
-
+    private void test() {
+        Room r = new Room(15, 34);
+        System.out.println("Within 20-30: " + r.withinPlan(new HeatingPlan(20, 30)));
+        System.out.println("Within 25-34: " + r.withinPlan(new HeatingPlan(25, 34)));
+        System.out.println("Within 10-40: " + r.withinPlan(new HeatingPlan(10, 40)));
+    }
 }
