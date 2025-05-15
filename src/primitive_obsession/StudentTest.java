1161d5b66c887c00f29cd36335ea153e20e814d7
Class <Class@ASM-A1106R1.binus.local>
2025-05-15 13:47:54 +0700

    Design Smell - banking init

--- src/primitive_obsession/StudentTest.java
+++ src/primitive_obsession/StudentTest.java
@@ -1,37 +1,37 @@
-package primitive_obsession;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class StudentTest {
-
-	@Test
-	void testSuccess() {
-		try {
-			Student m = new Student(new FullName("Amir Budianto"), StudentType.Reguler, new BirthDate(20, 11, 1999));	
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
-	}
-
-	@Test
-	void testErrorName() {
-		try {
-			Student m = new Student(new FullName("amir budianto123"), StudentType.Reguler, new BirthDate( 20, 11, 1999));	
-		} catch (Exception e) {
-			return;
-		}
-		fail("exception is expected");
-	}
-	
-	@Test
-	void testErrorDoB() {
-		try {
-			Student m = new Student(new FullName("Amir Budianto"), StudentType.Reguler, new BirthDate(37, 11, 1999));	
-		} catch (Exception e) {
-			return;
-		}
-		fail("exception is expected");
-	}
-}
+//package primitive_obsession;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class StudentTest {
+//
+//	@Test
+//	void testSuccess() {
+//		try {
+//			Student m = new Student(new FullName("Amir Budianto"), StudentType.Reguler, new BirthDate(20, 11, 1999));	
+//		} catch (Exception e) {
+//			fail(e.getMessage());
+//		}
+//	}
+//
+//	@Test
+//	void testErrorName() {
+//		try {
+//			Student m = new Student(new FullName("amir budianto123"), StudentType.Reguler, new BirthDate( 20, 11, 1999));	
+//		} catch (Exception e) {
+//			return;
+//		}
+//		fail("exception is expected");
+//	}
+//	
+//	@Test
+//	void testErrorDoB() {
+//		try {
+//			Student m = new Student(new FullName("Amir Budianto"), StudentType.Reguler, new BirthDate(37, 11, 1999));	
+//		} catch (Exception e) {
+//			return;
+//		}
+//		fail("exception is expected");
+//	}
+//}
